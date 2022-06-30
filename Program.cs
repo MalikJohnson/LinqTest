5902b124b583e36a463d0f2fb60e4f9302c5cef2
Malik Johnson <60333731+MalikJohnson@users.noreply.github.com>
2022-06-30 14:15:13 -0400

    Added word query
    Added a word query and added words to word list<string>
    
    ran a query for words that start with 't'

--- Program.cs
+++ Program.cs
@@ -8,15 +8,35 @@ var hashMap =   new Dictionary<int, string>();
 
 int[] testNum = { 50, 1, -1, 66, -80, 1000, 682, -972, 0, 30, 20, 66, 94, 28 };
 
+
+words.Add("hello");
+words.Add("test");
+words.Add("goodbye");
+words.Add("tree");
+words.Add("map");
+words.Add("stack");
+words.Add("queue");
+
 IEnumerable<int> testQuery =
     from num in testNum
-    where num > 20
+    where num < 0
     select num;
 
+IEnumerable<String> wordQuery =
+    from word in words
+    where word.StartsWith('t')
+    select word;
+
 // Execute the query.
-foreach (int z in testQuery)
+/*foreach (int z in testQuery)
 {
     Console.Write(z + " ");
+}*/
+
+foreach (string b in wordQuery)
+{
+    Console.Write(b + " ");
 }
 
-Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Hello, World!");
